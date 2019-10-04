45c474a97f60ca108f74c9f3856101d9ff238a83
jimanPark <im@gman.io>
2019-10-04 22:27:59 +0900

    set husky

--- src/RxApi.ts
+++ src/RxApi.ts
@@ -2,7 +2,9 @@ import fetch from 'node-fetch';
 import { of } from 'rxjs';
 
 const getUsers = fetch('https://koreanjson.com/users');
-of(getUsers).subscribe(async (requestApi: Request) => {
+
+of(getUsers).pipe(
+).subscribe(async (requestApi: Request) => {
   const data = await requestApi;
 
   console.log(await data.json());
